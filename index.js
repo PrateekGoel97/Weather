e7a09d0c7b358c90615b7c3c07cfdcd5c17ab7ac
Prateek Goel <goelprateek97@gmail.com>
2020-12-24 11:34:02 +0530

    Setting up express server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =  require('express');
+const port = 8000;
+
+
+
+const app = express();
+
+
+
+
+app.listen(port, function(err){
+
+    if(err){
+        console.log("error",err);
+        return;
+    }
+    console.log("express is running on port",port);
+});
