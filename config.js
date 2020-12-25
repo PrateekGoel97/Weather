08f588d98ec08ed02e9eeda68e857c6dc1b66e8a
Prateek Goel <goelprateek97@gmail.com>
2020-12-25 19:16:50 +0530

    Final project

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const constants = {
     openWeatherMap: {
         BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
-        SECRET_KEY:"768521670890b3cb52544bff97fe5356"
+        SECRET_KEY:"something"
     }
 }
 
